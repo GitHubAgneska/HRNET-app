07ba6b7ae0dd51cb52e1dbfc6c5525da27d7ba05
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2021-11-10 18:49:33 +0100

    set up base unit test for header component

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the appropriate header', () => {
+  const { getByText } =  render(<App />); // query is scoped to render's result
+  expect (getByText("HRnet")).toBeInTheDocument();
+  
+  
 });
