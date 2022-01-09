07621f626d598f34491e48aba85a9d3c01e76b10
Agneska <40915591+GitHubAgneska@users.noreply.github.com>
2022-01-09 06:32:17 +0100

    datatable > +jsdoc

--- src/components/elements/Table/Table.jsx
+++ src/components/elements/Table/Table.jsx
@@ -10,6 +10,14 @@ const Table = ({currentPageToDisplay, sortListBy}) => {
 
     const tableHead = [ 'firstName', 'lastName', 'dob', 'startDate', 'street', 'city', 'state', 'zipcode', 'department']
 
+    /** @function tableRow 
+     *  uses @type {Object} employee passed by @function tableData
+     *  and  @type {Array} tableHead
+     *  to generate @function columnData
+     *  @function columnData maps these together to produce @type{Markup} for each table data cell <td>
+     *  if needed, it processes data from employee object to prepare for render (@example using momentjs to format dates)
+     *  @returns @type{Markup} StyledTableRow : each table row corresponding to an employee object, mapped as table data cells
+    */
     const tableRow = employee => {
 
         // eslint-disable-next-line no-unused-vars
@@ -29,6 +37,11 @@ const Table = ({currentPageToDisplay, sortListBy}) => {
         return (<StyledTableRow key={Math.random()}>{columnData}</StyledTableRow>)
     }
 
+    /** @function tableData 
+     *  maps currentPage @type{employees[]} to key:index pairs
+     *  for @function tableRow to use
+     *  @returns {@function tableRow}
+    */
     const tableData = () => { return currentPageToDisplay.map((key, index) => tableRow({key, index}))  }
 
     return (
